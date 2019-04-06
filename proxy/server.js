d67d4a6cbb087ae44f86f513d91d1f5b02463e32
Andrew Myint <doku.potharhtoo@gmail.com>
2019-04-06 04:35:22 -0400

    testing with proxy server

---
+++ proxy/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 4000;
+const proxy = require('http-proxy-middleware');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  '/allReviews',
+  proxy({
+    target: 'http://localhost:9000',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/allItems',
+  proxy({
+    target: 'http://localhost:9000',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/test',
+  proxy({
+    target: 'http://localhost:3001',
+    changeOrigin: true
+  })
+)
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
+
