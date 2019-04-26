ec42d47a50322adabe479e080168a1d819f83c0c
Andrew Myint <doku.potharhtoo@gmail.com>
2019-04-26 01:56:24 -0400

    refactor service url with env

---
+++ proxy/config.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  andrew : process.env.ANDREW,
+  heather : process.env.HEATHER,
+  anait : process.env.ANAIT,
+  aarushi : process.env.AARUSHI
+}
--- proxy/server.js
+++ proxy/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const dotenv = require('dotenv');
+const config = require('./config.js');
+console.log(config);
+
 const app = express();
 const port = process.env.PORT || 4000;
 const proxy = require('http-proxy-middleware');
@@ -32,12 +36,15 @@ app.use(
   })
 )
 app.use(
-  '/test1',
+  '/ingredients',
   proxy({
     target: 'http://localhost:6001',
     changeOrigin: true
   })
 )
+app.get('/script', (req, res) => {
+  res.send(config);
+})
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
